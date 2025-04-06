031414ef1c0896bd15cc178d6eb6532bd9e26b04
saleem malik <saleemalik13@gmail.com>
2025-04-06 20:07:35 +0530

    .env file by decouple and connected with mysql

--- task_management/settings.py
+++ task_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-stg@^&7@3e42a4i2^ytr(lm$rf=hkgi^ehg4zfhx0gm0=v%3sg'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,6 +88,17 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': config('ENGINE'),
+        'NAME': config('NAME'),
+        'HOST': config('HOST'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'PORT': config('PORT')
+    }
+}
 
 
 # Password validation
