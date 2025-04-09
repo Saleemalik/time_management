043440cf3f34fc1eafb2c8f518ae131e92b20189
saleem malik <saleemalik13@gmail.com>
2025-04-09 19:54:20 +0530

    day set 1

--- task_management/settings.py
+++ task_management/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
 ]
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
 }
 
